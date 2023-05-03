34dbeb2ad149fb48d27eb62be975c9ea5f8f6d31
Anchal <anchal.somani05@gmail.com>
2023-05-03 16:36:14 +0530

    Add project files.

---
+++ azuresqlapp/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace azuresqlapp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ azuresqlapp/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using azuresqlapp.Models;
+using azuresqlapp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azuresqlapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public List<Product> Products;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+        }
+    }
+}
